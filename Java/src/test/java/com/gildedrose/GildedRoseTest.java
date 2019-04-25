b6ff2a9caf669bc159a38b6ad636f1e1e6f60392
lcdlv <lcdlv@lcdlv.fr>
2019-04-25 10:47:38 +0200

    add tests to cover code updateQuality for elixir and backtage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,7 +1,6 @@
 package com.gildedrose;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class GildedRoseTest {
@@ -11,7 +10,30 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+    @Test
+    public void  update_quality_elixir() {
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 7)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Elixir of the Mongoose", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+    }
+    @Test
+    public void  update_quality_backstage_passes() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 7)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
 
 }
