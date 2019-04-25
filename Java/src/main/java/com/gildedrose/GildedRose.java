173dfb95accd7b10606b372fb4bd54a9a15b9e78
lcdlv <lcdlv@lcdlv.fr>
2019-04-25 15:48:09 +0200

    refactor simplify methode

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,42 +14,26 @@ class GildedRose {
             String ItemName_Sulfuras = "Sulfuras, Hand of Ragnaros";
             int quality;
             int sellIn;
+            int unitOne = 1;
 
             if (!item.name.equals(ItemName_aged_brie)
                     && !item.name.equals(ItemName_Backstage)) {
                 quality = 0;
-                if (item.quality > quality) {
-                    if (!item.name.equals(ItemName_Sulfuras)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+                decreaseQualityAccordingToQuality(item, ItemName_Sulfuras, quality, unitOne);
             } else {
                 quality = 50;
                 if (item.quality < quality) {
-                    item.quality = item.quality + 1;
+                    increaseQualityByOne(item, unitOne);
 
                     if (item.name.equals(ItemName_Backstage)) {
-                        sellIn = 11;
-                        quality = 50;
-                        if (item.sellIn < sellIn) {
-                            if (item.quality < quality) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                        sellIn = 6;
-
-                        if (item.sellIn < sellIn) {
-                            quality = 50;
-                            if (item.quality < quality) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                        increaseQualityAccordingToSellIn(item, 50, 11, unitOne);
+                        increaseQualityAccordingToSellIn(item, 50, 6, unitOne);
                     }
                 }
             }
 
             if (!item.name.equals(ItemName_Sulfuras)) {
-                item.sellIn = item.sellIn - 1;
+                decreaseSellinByOne(item, unitOne);
             }
 
             sellIn = 0;
@@ -57,21 +41,47 @@ class GildedRose {
                 if (!item.name.equals(ItemName_aged_brie)) {
                     if (!item.name.equals(ItemName_Backstage)) {
                         quality = 0;
-                        if (item.quality > quality) {
-                            if (!item.name.equals(ItemName_Sulfuras)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+                        decreaseQualityAccordingToQuality(item, ItemName_Sulfuras, quality, unitOne);
                     } else {
                         item.quality = 0;
                     }
                 } else {
                     quality = 50;
-                    if (item.quality < quality) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQualityAccordingToQuality(item, quality, unitOne);
                 }
             }
+        }
+    }
+
+    private void increaseQualityByOne(Item item, int unitOne) {
+        item.quality = item.quality + unitOne;
+    }
+
+    private void decreaseQualityByOne(Item item, int unitOne) {
+        item.quality = item.quality - unitOne;
+    }
+
+    private void decreaseSellinByOne(Item item, int unitOne) {
+        item.sellIn = item.sellIn - unitOne;
+    }
+
+    private void increaseQualityAccordingToQuality(Item item, int quality, int unitOne) {
+        if (item.quality < quality) {
+            item.quality = item.quality + unitOne;
+        }
+    }
+
+    private void decreaseQualityAccordingToQuality(Item item, String itemName_Sulfuras, int quality, int unitOne) {
+        if (item.quality > quality) {
+            if (!item.name.equals(itemName_Sulfuras)) {
+                decreaseQualityByOne(item, unitOne);
+            }
+        }
+    }
+
+    private void increaseQualityAccordingToSellIn(Item item, int quality, int sellIn, int unitOne) {
+        if (item.sellIn < sellIn) {
+            increaseQualityAccordingToQuality(item, quality, unitOne);
         }
     }
 }
