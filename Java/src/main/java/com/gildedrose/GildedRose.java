c216e1369bdfb69682ef79417a1b13101ce60189
lcdlv <lcdlv@lcdlv.fr>
2019-04-29 11:57:47 +0200

    move methods about Item to ItemComposition Class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final int QUALITY_MAX = 50;
-    private static final int QUALITY_MIN = 0;
 
     Item[] items;
 
@@ -12,79 +10,10 @@ class GildedRose {
 
     void updateQuality() {
         for (Item item : items) {
-            updateQuality(ItemFactoryCopy.getItemFactory(item).item);
+            ItemComposition itemComposition = new ItemComposition(item);
+            itemComposition.updateQuality();
         }
     }
 
-    private void updateQuality(Item item) {
-        decreaseSellInIfNotSulfuras(item);
-        increaseQualityAgedBrie(item);
-        increaseQualityBackstage(item);
 
-        if(!ItemFactoryCopy.isAgedBrie(item)&& !ItemFactoryCopy.isBackstage(item)) {
-            decreaseQualityAccordingToQuality(item);
-        }
-
-        if (ItemFactoryCopy.isSellInPassed(item)) {
-            increaseQualityAgedBrie(item);
-
-            if(!ItemFactoryCopy.isAgedBrie(item)) {
-                decreaseQualityBackstage(item);
-            }
-        }
-    }
-
-    private void increaseQualityAgedBrie(Item item) {
-        if (ItemFactoryCopy.isAgedBrie(item)) {
-            if (item.quality < QUALITY_MAX) {
-                item.quality = item.quality + 1;
-            }
-        }
-    }
-
-    private void increaseQualityBackstage(Item item) {
-        if (ItemFactoryCopy.isBackstage(item)) {
-            if (item.quality < QUALITY_MAX) {
-                item.quality = item.quality + 1;
-                if (ItemFactoryCopy.isBackstage(item)) {
-                    increaseQualityAccordingToSellIn(item, 11);
-                    increaseQualityAccordingToSellIn(item, 6);
-                }
-            }
-        }
-    }
-
-    private void decreaseQualityBackstage(Item item) {
-        if (ItemFactoryCopy.isBackstage(item)) {
-            item.quality = QUALITY_MIN;
-        } else {
-            decreaseQualityAccordingToQuality(item);
-        }
-    }
-
-    private void decreaseSellInIfNotSulfuras(Item item) {
-        ItemFactoryCopy.isSulfuras(item);
-        decreaseSellInByOne(item);
-    }
-
-    private void decreaseSellInByOne(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private void decreaseQualityAccordingToQuality(Item item) {
-        if (item.quality > GildedRose.QUALITY_MIN) {
-            if (ItemFactoryCopy.isSulfuras(item)) {
-                return;
-            }
-            ItemFactoryCopy.decreaseQualityByOne(item);
-        }
-    }
-
-    private void increaseQualityAccordingToSellIn(Item item, int sellIn) {
-        if (item.sellIn < sellIn) {
-            if (item.quality < QUALITY_MAX) {
-                item.quality = item.quality + 1;
-            }
-        }
-    }
 }
---
+++ Java/src/main/java/com/gildedrose/ItemComposition.java
@@ -0,0 +1,102 @@
+package com.gildedrose;
+
+class ItemComposition {
+
+    private static final int QUALITY_MAX = 50;
+    private static final int QUALITY_MIN = 0;
+    private final Item item;
+
+    ItemComposition(Item item) {
+        this.item = item;
+    }
+
+    private boolean isBackstage() {
+        return item.name.equals(ItemType.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getValue());
+    }
+
+    private boolean isAgedBrie() {
+        return item.name.equals(ItemType.AGED_BRIE.getValue());
+    }
+
+    private boolean isSellInPassed() {
+        return item.sellIn < 0;
+    }
+
+    private void decreaseQualityByOne() {
+        item.quality = item.quality - 1;
+    }
+
+    private boolean isSulfuras() {
+        return item.name.equals(ItemType.SULFURAS_HAND_OF_RAGNAROS.getValue());
+    }
+
+    void updateQuality() {
+        decreaseSellInIfNotSulfuras();
+        increaseQualityAgedBrie();
+        increaseQualityBackstage();
+
+        if (!isAgedBrie() && !isBackstage()) {
+            decreaseQualityAccordingToQuality();
+        }
+
+        if (isSellInPassed()) {
+            increaseQualityAgedBrie();
+
+            if (!isAgedBrie()) {
+                decreaseQualityBackstage();
+            }
+        }
+    }
+
+    private void increaseQualityAgedBrie() {
+        if (isAgedBrie()) {
+            if (item.quality < QUALITY_MAX) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void increaseQualityBackstage() {
+        if (isBackstage()) {
+            if (item.quality < QUALITY_MAX) {
+                item.quality = item.quality + 1;
+                increaseQualityAccordingToSellIn(11);
+                increaseQualityAccordingToSellIn(6);
+            }
+        }
+    }
+
+    private void decreaseQualityBackstage() {
+        if (isBackstage()) {
+            item.quality = QUALITY_MIN;
+        } else {
+            decreaseQualityAccordingToQuality();
+        }
+    }
+
+    private void decreaseSellInIfNotSulfuras() {
+        isSulfuras();
+        decreaseSellInByOne();
+    }
+
+    private void decreaseSellInByOne() {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void decreaseQualityAccordingToQuality() {
+        if (item.quality > QUALITY_MIN) {
+            if (isSulfuras()) {
+                return;
+            }
+            decreaseQualityByOne();
+        }
+    }
+
+    private void increaseQualityAccordingToSellIn(int sellIn) {
+        if (item.sellIn < sellIn) {
+            if (item.quality < QUALITY_MAX) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+}
